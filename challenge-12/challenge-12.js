e9e9a7c44a5b673005cc9b7cde300764a4e81d17
manasseis <manasseis@gmail.com>
2020-08-02 22:41:49 -0300

    Update challenge-12.js

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,84 +1,11 @@
-/*
-Envolva todo o conteúdo desse arquivo em uma II
-FE.
-*/
-/*
-Crie um objeto chamado `person`, com as propriedades:
-    `name`: 
-   `lastname`: String
-    `age`: Number
-Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
-de valor para cada propriedade.
-*/
-// ?
-console.log( 'Propriedades de "person":' );
-/*
-Mostre no console, em um array, todas as propriedades do objeto acima.
-Não use nenhuma estrutura de repetição, nem crie o array manualmente.
-*/
-// ?
-/*
-Crie um array vazio chamado `books`.
-*/
-// ?
-/*
-Adicione nesse array 3 objetos, que serão 3 livros. Cada livro deve ter a
-seguintes propriedades:
-`name`: String
-`pages`: Number
-*/
-// ?
-console.log( '\nLista de livros:' );
-/*
-Mostre no console todos os livros.
-*/
-// ?
-console.log( '\nLivro que está sendo removido:' );
-/*
-Remova o último livro, e mostre-o no console.
-*/
-// ?
-console.log( '\nAgora sobraram somente os livros:' );
-/*
-Mostre no console os livros restantes.
-*/
-// ?
-/*
-Converta os objetos que ficaram em `books` para strings.
-*/
-// ?
-console.log( '\nLivros em formato string:' );
-
-/*
-Mostre os livros nesse formato no console:
-*/
-// ?
-
-/*
-Converta os livros novamente para objeto.
-*/
-// ?
-console.log( '\nAgora os livros são objetos novamente:' );
-/*
-no formato abaixo:
-    "[PROPRIEDADE]: [VALOR]"
-*/
-// ?
-/*
-Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
-seu nome. Adicione seu nome completo no array.
-*/
+k
+Mostre nokk
 // ?
 console.log( '\nMeu nome é:' );
 /*
-Juntando todos os itens do array, mostre no console seu nome.
-*/
-// ?
-console.log( '\nMeu nome invertido é:' );
-/*
-Ainda usando o objeto acima, mostre no console seu nome invertido.
-*/
+Juntando todos os itens do array, mostre no console seu nome.k
 // ?
 console.log( '\nAgora em ordem alfabética:' );
 /*
 Mostre todos os itens do array acima, odenados alfabéticamente.
+k
